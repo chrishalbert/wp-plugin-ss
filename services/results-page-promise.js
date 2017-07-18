cfd7a88b9ae20891dd85277ef73b91e58f475fb9
Chris Halbert <christopher.halbert@gmail.com>
2017-07-18 17:13:44 -0400

    Using native es6 promises rather than library.

--- services/results-page-promise.js
+++ services/results-page-promise.js
@@ -1,25 +1,26 @@
 const ResultsPage = require('../lib/results-page');
 const https = require('https');
-const Promise = require('bluebird');
 const winston = require('winston');
 
-module.exports = Promise.method(url => new Promise((resolve, reject) => {
-  const request = https.get(url, (response) => {
-    let body = '';
-    response.on('data', (data) => {
-      body += data;
+module.exports = function(url) {
+  return new Promise((resolve, reject) => {
+    const request = https.get(url, (response) => {
+      let body = '';
+      response.on('data', (data) => {
+        body += data;
+      });
+
+      response.on('end', () => {
+        resolve(new ResultsPage(body));
+      });
     });
 
-    response.on('end', () => {
-      resolve(new ResultsPage(body));
-      body = null;
+    request.on('error', (error) => {
+      reject(error);
+      winston.error(url, error);
+      reject(error);
     });
-  });
 
-  request.on('error', (error) => {
-    winston.error(url, error);
-    reject(error);
+    request.end();
   });
-
-  request.end();
-}));
+};
